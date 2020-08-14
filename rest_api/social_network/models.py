e22c9d6e65965ddff0df9501b228a5b2266a7e71
Taras Kutsiaba <tarikkus24@gmail.com>
2020-08-14 14:56:22 +0300

    aded models

--- rest_api/social_network/models.py
+++ rest_api/social_network/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=250)
+    second_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=254)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(blank=True, max_length=250)
+    like = models.PositiveSmallIntegerField()
+
+def __str__(self):
+        return self.title
